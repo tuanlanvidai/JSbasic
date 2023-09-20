45833cca40d67755ca9969641072448844472954
tuanlanvidai <lantruongtuan333@gmail.com>
2023-09-20 19:41:06 +0700

    a

---
+++ bai1.js
@@ -0,0 +1,53 @@
+//Bai 1
+//a
+// console.log('Hello world!')
+
+//b
+// console.log('Truong Tuan Lan')
+
+//c
+// console.clear()
+
+//d
+// console.log('*****')
+// console.log('*****')
+// console.log('*****')
+// console.log('*****')
+// console.log('*****')
+
+//e
+// console.log('     *     ')
+// console.log('    ***    ')
+// console.log('   *****   ')
+// console.log('  *******  ')
+// console.log(' ********* ')
+// console.log('***********')
+
+//f
+// console.log(' ** ** ')
+// console.log('*  *  *')
+// console.log('*     *')
+// console.log(' *   * ')
+// console.log('  * *  ')
+// console.log('   *   ')
+
+//BAI2
+//a
+// let a = 10
+// console.log(a)
+
+//b
+// let b = '10'
+// console.log(b)
+
+//c
+// a=20
+// console.log(a)
+
+//d
+const pi = 3.14
+console.log(pi)
+
+pi = 5
+console.log(pi) //bao loi
+
