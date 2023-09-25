9368451834c72cf95fa60837add5369191d946cf
tuanlanvidai <lantruongtuan333@gmail.com>
2023-09-25 18:44:27 +0700

    a

--- bai1.js
+++ bai1.js
@@ -52,16 +52,110 @@
 // console.log(pi) //bao loi
 
 //f - OBJECT
-let dienThoai = {
-    ten: "iPhone 14",
-    hangSanXuat: "Apple",
-    mauSac: "Trang",
-    gia: "1000 USD"
-}
-console.log(dienThoai)
-//g
-dienThoai.gia = "750 USD"
-console.log(dienThoai)
-//h
-dienThoai.ten = ("iPhone 14 promax")
-console.log(dienThoai)
+// let dienThoai = {
+//     ten: "iPhone 14",
+//     hangSanXuat: "Apple",
+//     mauSac: "Trang",
+//     gia: "1000 USD"
+// }
+// console.log(dienThoai)
+// //g
+// dienThoai.gia = "750 USD"
+// console.log(dienThoai)
+// //h
+// dienThoai.ten = ("iPhone 14 promax")
+// console.log(dienThoai)
+
+// ========== Toan tu ==============
+//  3a
+// let a = 8
+// let b = 7
+
+// let tong = a + b
+// console.log(tong)
+// let hieu = a - b
+// console.log(hieu)
+// let tich = a * b
+// console.log(tich)
+// let thuong = a/b
+// console.log(thuong)
+
+//3b
+// let a = 5
+// let b = 11
+// let tbc = (a+b)/2
+// console.log(tbc)
+
+//3c
+// let x=3
+// let y=4
+// let z=6
+
+// let kq1 = x*x + 2*x + 1
+// console.log("x^2 + 2x + 1 = ", kq1)
+
+// let kq2 = x*x*x - 3*x*y - 5*y + 3*y*y
+// console.log("x^3 - 3xy - 5y + 3y^2 = ", kq2)
+
+// let kq3 = (x*y)*(x*y) - 2*x*x*y + 13*y
+// console.log("(xy)^2 - 2x^2*y + 13y = ", kq3)
+
+// let kq4 =  4*x*x*x + 3*x*y + y*y - (2*x*x - 3*y)
+// console.log("4x^3 + 3xy + y^2 - (2x^2 - 3y) = ",kq4)
+
+// let kq5 = ((5*x*x) / (4*x*y)) + y*y
+// console.log("(5x^2 / 4xy) + y^2 = ", kq5)
+
+// let kq6 = x*x - (2*z*x*y) / (y*y) + 5*x - 2*y*y + 4*x*z*z*z + z*z*z
+// console.log("x^2 - 2zxy / y^2 + 5x - 2y^2 + 4xz^3 + z^3 = ", kq6)
+
+//3d
+// let dk = 5
+// let chuVi = 3.14 * dk
+// let bk = dk/2
+// let dienTich = 3.14 * bk * bk
+
+// console.log("Chu vi = ", chuVi)
+// console.log("Dien tich = ", dienTich)
+
+//3e
+// let CD = 15
+// let CR = 10
+// chuVi = (CD + CR) * 2
+// dienTich = CD * CR
+// console.log("Chu vi = ", chuVi)
+// console.log("Dien tich = ", dienTich)
+
+//3f
+// let quangDuong = 120
+// let thoiGianPhut = 130
+// let thoiGianGio = thoiGianPhut / 60
+// let vanToc = quangDuong / thoiGianGio
+// console.log("Van toc = ", vanToc)
+
+// 3g
+// let thoiGianPhut = 150
+// let thoigianGio = thoiGianPhut / 60
+// let vanToc = 60
+// let quangDuong = vanToc * thoigianGio
+// console.log("Quang duong = ", quangDuong)
+
+//3h
+// let vanToc = 15.2
+// let thoiGian = 3
+// let quangDuong = vanToc * thoiGian
+// console.log("Quang duong = ", quangDuong)
+
+//3i
+// let thoiGianA = 8*60+20
+// let thoiGianB = 11*60
+// let thoiGianDi = (thoiGianB - thoiGianA)/60
+// let vanToc = 42
+// let quangDuong = thoiGianDi * vanToc
+// console.log("Quang Duong AB = ", quangDuong)
+
+//3j
+// let theTichDuoi = (8+8+8)*6*5
+// let theTichTren = 8*6*5
+// let theTichTong = theTichDuoi + theTichTren
+// console.log("The tich: ",theTichTong)
