fda49c2a15ef4901f5e8d7d1e84d329e229ef1cd
tuanlanvidai <lantruongtuan333@gmail.com>
2023-09-26 20:52:50 +0700

    a

--- bai1_function.js
+++ bai1_function.js
@@ -1,33 +1,94 @@
-function tinhTong(a,b)
-{
-    const ketQua = a + b
-    return ketQua
-}
+// function tinhTong(a,b)
+// {
+//     const ketQua = a + b
+//     return ketQua
+// }
 
-function tinhHieu(a,b)
-{
-    const ketQua = a - b
-    return ketQua
-}
+// function tinhHieu(a,b)
+// {
+//     const ketQua = a - b
+//     return ketQua
+// }
 
-function tinhTich(a, b)
-{
-    const ketQua = a * b
-    return ketQua
-}
+// function tinhTich(a, b)
+// {
+//     const ketQua = a * b
+//     return ketQua
+// }
 
-function tinhThuong(a, b)
-{
-    const ketQua = a/b
-    return ketQua
-}
+// function tinhThuong(a, b)
+// {
+//     const ketQua = a/b
+//     return ketQua
+// }
 
-let kq1 = tinhTong(2, 5)
-let kq2 = tinhHieu(9, 3)
-let kq3 = tinhTich(2, 5)
-let kq4 = tinhThuong(10,2)
+// let kq1 = tinhTong(2, 5)
+// let kq2 = tinhHieu(9, 3)
+// let kq3 = tinhTich(2, 5)
+// let kq4 = tinhThuong(10,2)
 
-console.log(kq1)
-console.log(kq2)
-console.log(kq3)
-console.log(kq4)
+// console.log(kq1)
+// console.log(kq2)
+// console.log(kq3)
+// console.log(kq4)
+
+//== b ==
+// function TBC(a, b)
+// {
+//     let ketQua = (a + b) / 2
+//     return ketQua
+// }
+// let kq = TBC(5, 20)
+// console.log(kq)
+
+//  == d ==
+// function chuVi(d)
+// {
+//     let ketQua = d*3.14
+//     return ketQua
+// }
+// function dienTich(d)
+// {
+//     let r = d/2
+//     let ketQua = r*r*3.14
+//     return ketQua
+// }
+
+// let cv = chuVi(5)
+// let dt = dienTich(5)
+// console.log(cv, dt)
+
+// == e ==
+// function chuViHCN(cd, cr)
+// {
+//     const ketQua = (cd + cr)/2
+//     return ketQua
+// }
+// function dienTichHCN(cd, cr)
+// {
+//     const ketQua = cd * cr
+//     return ketQua
+// }
+// let cv = chuViHCN(15,10)
+// let dt = dienTichHCN(15,10)
+// console.log(cv, dt)
+
+//==f==
+// function vanToc(qd, tg)
+// {
+//     let thoiGianGio = tg/60
+//     const ketQua = qd / thoiGianGio
+//     return ketQua
+// }
+// let vt = vanToc(120, 130)
+// console.log(vt)
+
+// == g == 
+// function quangDuong(vt, tg)
+// {
+//     let thoiGianGio = tg / 60
+//     const ketQua = vt * thoiGianGio
+//     return ketQua
+// }
+// let qd = quangDuong(60, 150)
+// console.log(qd)
