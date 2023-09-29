1afb78b79a15ff5b186dfea394757bcb2401b569
tuanlanvidai <lantruongtuan333@gmail.com>
2023-09-29 19:43:24 +0700

    a

---
+++ bai3.js
@@ -0,0 +1,8 @@
+//bai1
+let arr = [3, 451, 5, 2, 6, 76, 3, 5, 1, 243, 6, 4]
+let doDaiMang = arr.length
+
+for(let i = 0; i< doDaiMang;  i++)
+{
+    console.log("Phan tu thu %s = %s", i, arr[i])
+}
