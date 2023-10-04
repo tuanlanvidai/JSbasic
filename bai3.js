6a98670ffbf5a8e981518e1b1a03b784a73be082
tuanlanvidai <lantruongtuan333@gmail.com>
2023-10-04 19:39:46 +0700

    a

--- bai3.js
+++ bai3.js
@@ -1,6 +1,6 @@
 //bai1
-let arr = [3, 451, 5, 2, 6, 76, 3, 5, 1, 243, 6, 4]
-let doDaiMang = arr.length
+// let arr = [3, 451, 5, 2, 6, 76, 3, 5, 1, 243, 6, 4]
+// let doDaiMang = arr.length
 
 // for(let i = 0; i< doDaiMang;  i++)
 // {
@@ -69,12 +69,12 @@ let doDaiMang = arr.length
 // }
 
 //g
-for(let i = doDaiMang-1; i>=0; i--)
-{
-    arr[i+1] = arr[i];
-}
-arr[0] = 117
-console.log(arr)
+// for(let i = doDaiMang-1; i>=0; i--)
+// {
+//     arr[i+1] = arr[i];
+// }
+// arr[0] = 117
+// console.log(arr)
 
 //h
 // let phanTuHienTai = arr[0]
@@ -99,14 +99,89 @@ console.log(arr)
 // }
 
 //i
-let bienTam=0
+// let bienTam=0
+// console.log(arr)
+// for(let i = 0; i<doDaiMang-1; i++)
+// {
+//     for(let j = i+1; j<doDaiMang; j++)
+//     {
+//         if(arr[i]>arr[j])
+//         {
+//             bienTam = arr[i]
+//             arr[i] = arr[j]
+//             arr[j] = bienTam
+//         }
+//     }
+// }
+// console.log(arr)
+
+//j
+// let bienTam = 0
+// console.log(arr)
+// for(let i=0; i<doDaiMang-1; i++)
+// {
+//     for(let j = i+1; j<doDaiMang; j++)
+//     {
+//         if(arr[i]< arr[j])
+//         {
+//             bienTam=arr[i]
+//             arr[i]= arr[j]
+//             arr[j] = bienTam
+//         }
+//     }
+// }
+// console.log(arr)
+
+//k
+// console.log("Danh sach so chan: ")
+// for(let i=0; i<doDaiMang; i++)
+// {
+//     if(arr[i]%2 ==0)
+//     {
+//         console.log(arr[i])
+//     }
+// }
+
+//l
+// console.log("Danh sach so le")
+// for(let i=0; i<doDaiMang; i++)
+// {
+//     if(arr[i]%2 !=0)
+//     {
+//         console.log(arr[i])
+//     }
+// }
+
+// == bai 2
+let arr = [5,-2,5,-6,23,69,100,-13,10,0,14]
+let doDaiMang = arr.length
+
+//a
+// for(let i = 0; i < doDaiMang-1; i++)
+// {
+//     for(let j = i+1; j<doDaiMang; j++)
+//     {
+//         if(arr[i] + arr[j] == 10)
+//         {
+//             console.log("[%s, %s]",arr[i], arr[j])
+//         }
+//     }
+// }
+
+//b
+console.log(arr)
 for(let i = 0; i<doDaiMang-1; i++)
 {
-    if(arr[i]>arr[i+1])
+    for(let j = i+1; j<doDaiMang; j++)
     {
-        bienTam = arr[i]
-        arr[i]= arr[i+1]
-        arr[i+1]=bienTam
+        if(arr[i]==arr[j])
+        {
+            console.log("Phan tu trung nhau: ", arr[i])
+            for(j; j<=doDaiMang;j++)
+            {
+                arr[j] = arr[j+1]
+            }
+        }
     }
 }
 console.log(arr)
